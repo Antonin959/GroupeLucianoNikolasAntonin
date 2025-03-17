d3b91a0541ba1b723b7df64e09b1aba8a4f2a1f6
Antonin959 <150444081+Antonin959@users.noreply.github.com>
2025-03-17 15:47:51 +0100

    Reapply "Sons de déplacement"
    This reverts commit 224c205374d0ec6c5d67b73ea31644932748f424.

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
-
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine.Audio;
 public class PlayerScript : MonoBehaviour
 {
+    public AudioClip[] MarcheHerbe, MarcheBois;
+    AudioSource audioSource;
+
     public Transform spawnpos;
     public Animator mirroir;
 
-    bool isEntranceOpen = false, isMirrorVisible = false;
+    bool isEntranceOpen = false, isMirrorVisible = false, isWalking = false;
 
     float rotX;
     public float speed = 2.0f;
@@ -15,8 +20,11 @@ public class PlayerScript : MonoBehaviour
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         cam = transform.GetChild(0);
+        StartCoroutine(PlayRandomClips());
+
     }
     void Update()
     {
@@ -24,6 +32,8 @@ public class PlayerScript : MonoBehaviour
         else speed = 0.6f;
 
         rb.linearVelocity = (Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right) * speed + new Vector3(0, rb.linearVelocity.y, 0);
+
+        isWalking = Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0;
 
         rotX -= Input.GetAxis("Mouse Y") * sensi;
         rotX = Mathf.Clamp(rotX, -90, 90);
@@ -48,6 +58,24 @@ public class PlayerScript : MonoBehaviour
             isMirrorVisible = !isMirrorVisible;
             mirroir.SetBool("is", isMirrorVisible);
         }
+    }
+
+    IEnumerator PlayRandomClips()
+    {
+        bool onHerbe = Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit) && hit.transform.tag == "GroundHerbe";
+
+        AudioClip clip = onHerbe ?MarcheHerbe[Random.Range(0, MarcheHerbe.Length)] : MarcheBois[Random.Range(0, MarcheBois.Length)];
+        audioSource.clip = clip;
+        if (isWalking)
+        {
+            Debug.Log(isWalking);
+            audioSource.Play();
+        }
+
+        yield return new WaitForSeconds(clip.length);
+
+        StartCoroutine(PlayRandomClips());
+
     }
 
     void OnTriggerEnter(Collider other)
